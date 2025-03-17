851d052c7ca297005a509d01bc2f4a27deadd601
KaterineOrjuela29 <100591753+KaterineOrjuela29@users.noreply.github.com>
2025-03-17 18:23:11 -0500

    configuracion

---
+++ config/Config.php
@@ -0,0 +1,17 @@
+<?php
+const BASE_URL = "http://localhost/aplicativo-web-pijamas-shalom-MonedaAndFixes/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "aplicativo_tienda_shalom";
+const CHARSET = "charset=utf8";
+const TITLE = "Shalom Pijamas";
+const MONEDA = "COP";
+const CLIENT_ID = "AQHEaKqx7wGj892mdHzChyAoHtUBbttQB0cGRdSenDHd60EUpBAfb-5oZ8XTnc83wPPKticfsAAtwN2E";
+
+const USER_SMTP = "pijamas.shalom.notificaciones@gmail.com";
+const PASS_SMTP = "npdpfrihvvcxbkxd";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
+
