f2d4dc002358a4eee0e8c8b03b936712b6ae0f37
KaterineOrjuela29 <100591753+KaterineOrjuela29@users.noreply.github.com>
2025-03-17 18:57:00 -0500

    prueba

---
+++ Views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <title>Shalom Pijamas</title>
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css">
+</head>
+ 
+<body>
+ 
+<div class="banner_bg_main">
+      <!-- logo section start -->
+      <div style="background-color:#F4C2C2" class="logo_section">
+         <div style="background-color:#F4C2C2"class="container">
+            <div class="row">
+               <div class="col-sm-12">
+                  <div class="logo"><a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL; ?>assets/images/shalom-logo.png" width="130"></a></div>
+               </div>
+            </div>
+         </div>
+      </div>
+    
+</body>
+ 
+</html>
