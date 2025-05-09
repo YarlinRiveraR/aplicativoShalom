1ec4908ba255a9aae01232c67f58f703e15becc1
KaterineOrjuela29 <100591753+KaterineOrjuela29@users.noreply.github.com>
2025-05-09 17:14:54 -0500

    icono para acceder al carrito

--- Views/template/header-secundario.php
+++ Views/template/header-secundario.php
@@ -134,6 +134,10 @@
                <div class="header_box">
                   <div class="login_menu">
                      <ul>
+                        <li><a href="#" id="verCarrito">
+                              <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                              <span class="padding_10" id="btnCantidadCarrito">Cart</span></a>
+                        </li>
                         <!-- NEW!!! -->
                         <li><a href="<?php echo BASE_URL . 'principal/deseo' ?>">
                            <i class="fas fa-fw fa-heart" aria-hidden="true"></i>
