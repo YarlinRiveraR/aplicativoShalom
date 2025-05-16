80dd143cfdb5c3283f72cd446c1d4d516abe1cc9
Gonsu <100593698+Gonsu@users.noreply.github.com>
2025-05-16 17:16:27 -0500

    Se añadio la funcion GetProductos y los ProductosMinimos y Top productos, estas son as estadisticas en el modelo para el administrador

--- Models/AdminModel.php
+++ Models/AdminModel.php
@@ -15,6 +15,23 @@ class AdminModel extends Query{
         $sql = "SELECT COUNT(*) AS total FROM pedidos WHERE proceso = $estado";
         return $this->select($sql);
     }
+    public function getProductos()
+    {
+        $sql = "SELECT COUNT(*) AS total FROM productos WHERE estado = 1";
+        return $this->select($sql);
+    }
+    
+    public function productosMinimos()
+    {
+        $sql = "SELECT * FROM productos WHERE cantidad < 15 AND estado = 1 ORDER BY cantidad DESC LIMIT 3";
+        return $this->selectAll($sql);
+    }
+
+    public function topProductos()
+    {
+        $sql = "SELECT producto, SUM(cantidad) AS total FROM detalle_pedidos GROUP BY id_producto ORDER BY total DESC LIMIT 3";
+        return $this->selectAll($sql);
+    }
     
     // Actualiza el token de recuperación para el usuario identificado por su correo
     public function updateToken($correo, $token) {
