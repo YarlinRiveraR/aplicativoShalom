3320111f5a2b5a2df3d74eb97e8d5bd320eafb16
Gonsu <100593698+Gonsu@users.noreply.github.com>
2025-05-16 17:19:15 -0500

    Se añadio el controlador para los modelos de las estadisticas de productos del administrador

--- Controllers/Admin.php
+++ Controllers/Admin.php
@@ -162,11 +162,38 @@ public function resetPassword($token) {
             exit;
         }
         $data['title'] = 'administracion';
-        
-        
+        $data['pendientes'] = $this->model->getTotales(1);
+        $data['procesos'] = $this->model->getTotales(2);
+        $data['finalizados'] = $this->model->getTotales(3);
+        $data['productos'] = $this->model->getProductos();
         $this->views->getView('admin/administracion', "index", $data);
     }
 
+    //obtener los productos con cantidades mínimas
+    public function productosMinimos()
+    {
+        if (empty($_SESSION['nombre_usuario'])) {
+            header('Location: '. BASE_URL . 'admin');
+            exit;
+        }
+        $data = $this->model->productosMinimos();
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+
+    }
+
+    //obtener los productos más vendidos
+    public function topProductos()
+    {
+        if (empty($_SESSION['nombre_usuario'])) {
+            header('Location: '. BASE_URL . 'admin');
+            exit;
+        }
+        $data = $this->model->topProductos();
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+        die();
+
+    }
     
     // Método para cerrar la sesión del usuario
     public function salir()
