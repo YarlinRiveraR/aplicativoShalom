8aa3cd71c9d01d16ed7b8fab3c16f95fa44b6088
Arguellis17 <arguelloj866@gmail.com>
2025-05-16 19:12:33 -0500

    Se agrego el modelo para el pedido de registros del cliente

--- Models/ClientesModel.php
+++ Models/ClientesModel.php
@@ -60,6 +60,34 @@ class ClientesModel extends Query{
         return $res;
     }
 
+    public function registrarDetalle($producto, $precio, $cantidad, $id_pedido, $id_producto)
+    {
+        $sql = "INSERT INTO detalle_pedidos (producto, precio, cantidad, id_pedido, id_producto) VALUES (?,?,?,?,?)";
+        $datos = array($producto, $precio, $cantidad, $id_pedido, $id_producto);
+        $data = $this->insertar($sql, $datos);
+        if ($data > 0) {
+            $res = $data;
+        } else {
+            $res = 0;
+        }
+        return $res;
+    }
+    public function getPedidos($id_cliente)
+    {
+        $sql = "SELECT * FROM pedidos WHERE id_cliente = $id_cliente";
+        return $this->selectAll($sql);
+    }
+    public function getPedido($idPedido)
+    {
+        $sql = "SELECT * FROM pedidos WHERE id = $idPedido";
+        return $this->select($sql);
+    }
+    public function verPedidos($idPedido)
+    {
+        $sql = "SELECT d.* FROM pedidos p INNER JOIN detalle_pedidos d ON p.id = d.id_pedido WHERE p.id = $idPedido";
+        return $this->selectAll($sql);
+    }
+
     public function getProducto($id_producto)
     {
         $sql = "SELECT * FROM productos WHERE id = $id_producto";
