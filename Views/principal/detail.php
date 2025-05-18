64a7298e437e9a5295a8053d5feaaa5d8f926f58
YarlinRiveraR <yarlinanyelisrr@ufps.edu.co>
2025-05-17 20:20:21 -0500

    Revert "fix detail.php"
    This reverts commit 6417f82ca089068bdde8ff77dd0726896e8c048b.

--- Views/principal/detail.php
+++ Views/principal/detail.php
@@ -1,5 +1,6 @@
 <?php include_once 'Views/template/header-secundario.php'; ?>
 
+
 <!-- Open Content -->
 <section class="bg-light">
     <div class="container pb-5">
@@ -8,13 +9,14 @@
                 <div class="card mb-3">
                     <img class="card-img img-fluid" src="<?php echo BASE_URL . $data['producto']['imagen']; ?>" alt="Card image cap" id="product-detail">
                 </div>
+
             </div>
+            <!-- col end -->
             <div class="col-lg-7 mt-5">
                 <div class="card">
                     <div class="card-body">
                         <h1 class="h2"><?php echo $data['producto']['nombre']; ?></h1>
                         <p class="h3 py-2">$ <?php echo number_format($data['producto']['precio'], 0, '', '.') . ' ' . MONEDA; ?></p>
-
                         <ul class="list-inline">
                             <li class="list-inline-item">
                                 <h6>Categoria</h6>
@@ -26,7 +28,6 @@
 
                         <h6>Descripción:</h6>
                         <p><?php echo $data['producto']['descripcion']; ?></p>
-
                         <form action="" method="GET">
                             <input type="hidden" id="idProducto" value="<?php echo $data['producto']['id']; ?>">
                             <div class="row">
@@ -66,19 +67,19 @@
                                 </div>
                             </div>
                         </form>
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<!-- Close Content -->
 
 <!-- Start Article -->
 <section class="py-5">
     <div class="container">
         <div class="row text-left p-2 pb-3">
-            <h4 class="text-success" style="font-size: 28px; font-weight: bold;">Productos Relacionados</h4>
+            <h4>Productos Relacionados</h4>
         </div>
         
         <!--Start Carousel Wrapper-->
@@ -86,7 +87,7 @@
             <?php foreach ($data['relacionados'] as $producto) { ?>
                 <div class="p-2 pb-3">
                     <div class="product-wap card rounded-0">
-                        <div class="card rounded-0 card-prod position-relative">
+                        <div class="card rounded-0">
                             <img class="card-img rounded-0 img-fluid" src="<?php echo BASE_URL . $producto['imagen']; ?>">
                             <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
                                 <ul class="list-unstyled">
@@ -100,16 +101,18 @@
                         </div>
                         <div class="card-body">
                             <h4 href="<?php echo BASE_URL . 'principal/detail/' . $producto['id']; ?>" class="h3 text-decoration-none"><?php echo $producto['nombre']; ?></h4>
-                            <p class="text-center mb-0">$ <?php echo number_format($producto['precio'], 0, '', '.') . ' ' . MONEDA; ?></p>
+                            <p class="text-center mb-0">$ <?php echo $producto['precio'] . ' ' . MONEDA; ?></p>
                         </div>
                     </div>
                 </div>
             <?php } ?>
         </div>
+
+
     </div>
 </section>
+<!-- End Article -->
 
-<!-- CSS personalizado -->
 <style>
   /* Título "Productos Relacionados" en negro */
   .text-success {
@@ -218,25 +221,26 @@
   }
 
   .product-wap img {
-        max-height: 200px; /* Ajustar según sea necesario */
-        object-fit: contain;
-        width: 100%;
-    }
+    max-height: 200px;
+    object-fit: contain;
+    width: 100%;
+  }
 
-    /* Establecer un tamaño fijo para el cuerpo de la tarjeta */
-    .product-wap .card-body {
-        height: 100px; /* Ajustar según sea necesario */
-        overflow: hidden;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
+  .product-wap .card-body {
+    height: 100px;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 </style>
+
 
 <?php include_once 'Views/template/footer-secundario.php'; ?>
 
 <script src="<?php echo BASE_URL; ?>assets/js/modulos/detail.js"></script>
 <script src="<?php echo BASE_URL; ?>assets/js/modulos/listaDeseo.js"></script>
+<!-- Start Slider Script -->
 <script src="<?php echo BASE_URL; ?>assets/js/slick.min.js"></script>
 <script>
     $('#carousel-related-product').slick({
@@ -245,12 +249,32 @@
         slidesToShow: 4,
         slidesToScroll: 3,
         dots: true,
-        responsive: [
-            { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 3 } },
-            { breakpoint: 600, settings: { slidesToShow: 2, slidesToScroll: 3 } },
-            { breakpoint: 480, settings: { slidesToShow: 2, slidesToScroll: 3 } }
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 3
+                }
+            }
         ]
     });
 </script>
+<!-- End Slider Script -->
+
 </body>
+
 </html>
