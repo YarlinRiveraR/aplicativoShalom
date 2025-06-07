5e6d2d5ed92a06049b960075a57edb6c8a5abd81
KaterineOrjuela29 <100591753+KaterineOrjuela29@users.noreply.github.com>
2025-06-06 19:09:21 -0500

    guardar el comprobante y cambio de estado

--- Models/PedidosModel.php
+++ Models/PedidosModel.php
@@ -1,15 +1,18 @@
 <?php
-class PedidosModel extends Query{
- 
+
+class PedidosModel extends Query
+{
     public function __construct()
     {
         parent::__construct();
     }
+
     public function getPedidos($proceso)
     {
         $sql = "SELECT * FROM pedidos WHERE proceso = $proceso";
         return $this->selectAll($sql);
     }
+
     public function actualizarEstado($proceso, $idPedido)
     {
         $sql = "UPDATE pedidos SET proceso=? WHERE id = ?";
@@ -17,7 +20,12 @@ class PedidosModel extends Query{
         return $this->save($sql, $array);
     }
 
-    
-}
+    // ✅ NUEVO: Guarda comprobante y cambia estado
+    public function actualizarComprobante($idPedido, $ruta, $nuevoEstado)
+    {
+        $sql = "UPDATE pedidos SET comprobante = ?, proceso = ? WHERE id = ?";
+        $datos = [$ruta, $nuevoEstado, $idPedido];
+        return $this->save($sql, $datos);
+    }
+
  
-?>
