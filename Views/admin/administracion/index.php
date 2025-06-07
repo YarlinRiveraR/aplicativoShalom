216db853f7dd2c1893631f9ae7b25c4925926478
YarlinRiveraR <100500377+YarlinRiveraR@users.noreply.github.com>
2025-06-06 19:51:38 -0500

    Add script para mostrar las estadísticas de pedidos en Admin

--- Views/admin/administracion/index.php
+++ Views/admin/administracion/index.php
@@ -102,6 +102,64 @@
 
 <?php include_once 'Views/template/footer-admin.php'; ?>
 
+<script>
+    // chart 2
+
+    var ctx = document.getElementById("reportePedidos").getContext('2d');
+
+    var gradientStroke1 = ctx.createLinearGradient(0, 0, 0, 300);
+    gradientStroke1.addColorStop(0, '#fc4a1a');
+    gradientStroke1.addColorStop(1, '#f7b733');
+
+    var gradientStroke2 = ctx.createLinearGradient(0, 0, 0, 300);
+    gradientStroke2.addColorStop(0, '#4776e6');
+    gradientStroke2.addColorStop(1, '#8e54e9');
+
+
+    var gradientStroke3 = ctx.createLinearGradient(0, 0, 0, 300);
+
+    gradientStroke3.addColorStop(0, '#42e695');
+    gradientStroke3.addColorStop(1, '#3bb2b8');
+
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["Pendientes", "Proceso", "Finalizados"],
+            datasets: [{
+                backgroundColor: [
+                    gradientStroke1,
+                    gradientStroke2,
+                    gradientStroke3
+                ],
+                hoverBackgroundColor: [
+                    gradientStroke1,
+                    gradientStroke2,
+                    gradientStroke3
+                ],
+                data: [<?php echo $data['pendientes']['total']; ?>,
+                    <?php echo $data['procesos']['total']; ?>,
+                    <?php echo $data['finalizados']['total']; ?>
+                ],
+                borderWidth: [1, 1, 1]
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            cutoutPercentage: 75,
+            legend: {
+                position: 'bottom',
+                display: false,
+                labels: {
+                    boxWidth: 8
+                }
+            },
+            tooltips: {
+                displayColors: false,
+            }
+        }
+    });
+</script>
+
 <script src="<?php echo BASE_URL; ?>assets/js/index.js"></script>
 </body>
 
