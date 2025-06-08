e2c6f30e3a46beb5b510558d7fbc324614ca19da
KaterineOrjuela29 <100591753+KaterineOrjuela29@users.noreply.github.com>
2025-06-07 21:44:47 -0500

    Arreglar nuevamente el cambiar estado del pedido y ver el pedido de los clientes

--- assets/js/modulos/pedidos.js
+++ assets/js/modulos/pedidos.js
@@ -4,20 +4,30 @@ const myModal = new bootstrap.Modal(document.getElementById("modalPedidos"));
 
 document.addEventListener("DOMContentLoaded", function() {
     tblPendientes = $("#tblPendientes").DataTable({
+        ajax: {
+            url: base_url + "pedidos/listarPedidos",
+            dataSrc: "",
+        },
         columns: [
-            { data: "id_transaccion" },
-            { data: "monto" },
-            { data: "estado" },
-            { data: "fecha" },
-            { data: "email" },
-            { data: "nombre" },
-            { data: "accion" },
-        ],
+    { data: "id_transaccion" },
+    { data: "monto" },
+    { data: "estado" },
+    { data: "fecha" },
+    { data: "email" },
+    { data: "nombre" },
+    { data: "comprobante" }, // <--- nueva columna
+    { data: "accion" }
+],
+
         language,
         dom,
         buttons,
     });
     tblProceso = $("#tblProceso").DataTable({
+        ajax: {
+            url: base_url + "pedidos/listarProceso",
+            dataSrc: "",
+        },
         columns: [
             { data: "id_transaccion" },
             { data: "monto" },
@@ -32,6 +42,10 @@ document.addEventListener("DOMContentLoaded", function() {
         buttons,
     });
     tblFinalizados = $("#tblFinalizados").DataTable({
+        ajax: {
+            url: base_url + "pedidos/listarFinalizados",
+            dataSrc: "",
+        },
         columns: [
             { data: "id_transaccion" },
             { data: "monto" },
@@ -102,4 +116,3 @@ function verPedido(idPedido) {
         }
     }
 }
-
